b9eb5804ca9edd8d5a75a8e7fea6a4d491d77435
Lucas-dev98 <l.o.bastos@live.com>
2024-10-20 13:59:02 -0300

    Update SignIn Module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,24 +1,35 @@
-import { Component, EventEmitter, Output, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MessageComponent } from './messages/message.component';  
-import { Message } from './messages/message.model';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { SigninComponent } from './auth/signin.component'; // Importe o SigninComponent
+import { MessageComponent } from './messages/message.component';
 import { MessageListComponent } from './messages/message-list.component';
 import { MessageInputComponent } from './messages/message-input.component';
 import { HeadersComponent } from './header.component';
-import { RouterOutlet } from '@angular/router';
+import { Message } from './messages/message.model'; // Add this line to import Message
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MessageComponent,MessageListComponent, MessageInputComponent,RouterOutlet,HeadersComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterOutlet,
+    SigninComponent, // Adicione o SigninComponent aos imports
+    MessageComponent,
+    MessageListComponent,
+    MessageInputComponent,
+    HeadersComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = "front-end";
 
-  mostrarElemento: boolean= true;
-  onMudaMostrarElemento(){
+  mostrarElemento: boolean = true;
+  onMudaMostrarElemento() {
     this.mostrarElemento = !this.mostrarElemento;
   }
 
--- frontend/src/app/auth/signin.component.ts
+++ frontend/src/app/auth/signin.component.ts
@@ -1,10 +1,21 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-signin',
-    standalone: true,
-    templateUrl: './signin.component.html'
+  selector: 'app-signin',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './signin.component.html'
 })
 export class SigninComponent {
-    
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    // Lógica para lidar com o envio do formulário
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    // Adicione aqui a lógica de autenticação
+  }
 }
