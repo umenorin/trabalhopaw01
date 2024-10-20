6578cf5384d9861720efc1e1fca39a3cce6851ce
Lucas-dev98 <l.o.bastos@live.com>
2024-10-20 15:10:22 -0300

    Update in app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,26 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
-import { SigninComponent } from './auth/signin.component';
-import { SignupComponent } from './auth/signup.component';
-import { LogoutComponent } from './auth/logout.componrnt';
-import { MessageListComponent } from './messages/message-list.component'; // Importar o componente de mensagens
+import { MessageComponent } from './messages/messages.component';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.components';
 import { AUTH_ROUTES } from './auth/auth.roulers';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/signin', pathMatch: 'full' },
-  { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'logout', component: LogoutComponent },
-  { path: 'messages', component: MessageListComponent }, // Adicionar a rota de mensagens
-  { path: 'user', title: 'Autenticação', children: AUTH_ROUTES, component: AuthenticationComponent },
-  { path: '**', component: PageNotFoundComponent }
+    {path: '', redirectTo:'mensagens', pathMatch:'full'},
+    {path: 'mensagens', 'title':'Mensagens', component:MessageComponent},
+    {path: 'user', 'title':'Autenticação',children: AUTH_ROUTES,component:AuthenticationComponent},
+    {path:'**',component:PageNotFoundComponent}
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+ 
